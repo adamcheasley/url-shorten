c3f1cbd0d1b518bcbd1b181233b2da656a10b5be
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2015-12-14 17:29:56 +0000

    shortcode is nullable

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -6,7 +6,7 @@ class URLMap(models.Model):
     """Map a shortened URL to the original URL
     """
     original_url = models.TextField()
-    shortcode = models.CharField(max_length=255, editable=False)
+    shortcode = models.CharField(max_length=255, editable=False, null=True)
 
     def save(self):
         self.shortcode = hashlib.sha256(
