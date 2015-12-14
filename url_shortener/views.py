51fa81364bb755d12991a941bc4c2e10d78770b9
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2015-12-14 17:30:11 +0000

    complete the resolve view

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import URLCreationForm
+from .models import URLMap
 
 
 def home(request):
@@ -12,8 +17,25 @@ def resolve(request, shortcode=None):
         return HttpResponseNotFound("<h1>Not found</h1>")
 
     # look up the real url
-    return HttpResponse("hello world")
+    url = URLMap.objects.filter(shortcode=shortcode)
+    if not url:
+        return HttpResponseNotFound("<h1>Not found</h1>")
+
+    return HttpResponseRedirect(url[0].original_url)
 
 
 def create_short_url(request):
-    return HttpResponse("create URL here")
+    if request.method == "POST":
+        form = URLCreationForm(request.POST)
+        form.is_valid()
+        new = URLMap(original_url=form.cleaned_data.get('url'))
+        new.save()
+        base_url = request.build_absolute_uri().replace(
+            request.get_full_path(), '')
+        return HttpResponse(
+            "Shortened URL is {}/resolve/{}".format(
+                base_url, new.shortcode))
+    else:
+        form = URLCreationForm()
+
+    return render(request, 'url_creation.html', {'form': form})
