0dd7645fbda25a04e48164da0d6064be5cfb9526
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2016-02-10 16:53:14 +0000

    add extremely simple test

--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from url_shortener.models import URLMap
 
-# Create your tests here.
+
+class BaseTestCase(TestCase):
+
+    def test_the_tests(self):
+        URLMap.objects.create(
+            original_url="http://someplacenice.co.uk")
